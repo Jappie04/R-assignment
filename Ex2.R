07913273fda866c52fe7d02c3a7373018ed56c48
Jappie_B <jasper.buijnink@gmail.com>
2026-06-16 16:37:46 +0200

    ex 2 done

--- Ex2.R
+++ Ex2.R
@@ -8,25 +8,41 @@ plot(data$V1, type="l")
 
 x <- ts(data)
 x.detrend <- detrend(x, tt = "linear", bp = c())
-x.detrend.scaled <- x.detrend / 20
+N <- length(x.detrend)
+x.detrend.scaled <- x.detrend / N
 autoplot(x.detrend.scaled)+xlab("x-axis")+ylab("y-axis")
 acfx <- acf(x.detrend.scaled)
 acfx <- ts(acfx$acf)
 ac <- as.numeric(acfx)
 acs <- t(ac) * ac
-N <- length(x.detrend.scaled)
-a <- rep(N, 20)
-for (j in 2:20){
+N.acfx <- length(acfx)
+a <- rep(NA, N.acfx)
+for (j in 2:N.acfx){
   a[j] = 0
   for (i in 1:j){
     a[j] = acs[i] + a[j]
   }
 }
 a[1] <- 0
-b <- sqrt((rep(1, 20)+2*a)/N)*qnorm(1 - 0.25 / 2)
+b <- sqrt((rep(1, N.acfx)+2*a)/N)*qnorm(1-0.05/2)
 autoplot(acfx)+xlab("lag") + ylab("autocorrelation")+theme(legend.position = "None")+
   theme(axis.title=element_text(size=18))+
   geom_line(aes(y = b), linetype='dashed', col = 'red')+
   geom_line(aes(y = -b), linetype='dashed', col = 'red')
 
+pacfx = pacf(x.detrend.scaled)
+pacfx = ts(pacfx$acf)
+N.pacfx <- length(pacfx)
+autoplot(pacfx)+xlab("lag") + ylab("partial correlation")+theme(legend.position = "None")+
+theme(axis.title=element_text(size=18))+
+  geom_line(aes(y = b[1:N.pacfx]), linetype='dashed', col = 'red')+
+  geom_line(aes(y = -b[1:N.pacfx]), linetype='dashed', col = 'red')
 
+arima(x.detrend.scaled, order = c(7,0,0), include.mean=FALSE)
+arima(x.detrend.scaled, order = c(8,0,0), include.mean=FALSE)
+arima(x.detrend.scaled, order = c(8,0,1), include.mean=FALSE)
+arima(x.detrend.scaled, order = c(9,0,0), include.mean=FALSE)
+arima(x.detrend.scaled, order = c(10,0,0), include.mean=FALSE)
+arima(x.detrend.scaled, order = c(30,0,0), include.mean=FALSE)
+arima(x.detrend.scaled, order = c(1,0,0), include.mean=FALSE)
+arima(x.detrend.scaled, order = c(1,0,1), include.mean=FALSE)
