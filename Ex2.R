a021d2c6b9c9bb866c168324c3b1621ccd108196
Jappie_B <jasper.buijnink@gmail.com>
2026-06-18 15:41:10 +0200

    added a way to print all possible arma models with order less than or equal to 5. cleaned up the code a bit.

--- Ex2.R
+++ Ex2.R
@@ -4,12 +4,14 @@ library("pracma")
 library("ggplot2")
 
 data <- read.table("arma_series_group_Jasper_Lieke.txt")
-autoplot(ts(data$V1[200:500]))+xlab("Time")+ylab("Value")
+autoplot(ts(data$V1))+xlab("Time")+ylab("Value")
 
 x <- ts(data)
+mean(x)
 x.detrend <- detrend(x, tt = "linear", bp = c())
 N <- length(x.detrend)
-x.detrend.scaled <- x.detrend / 1
+mean(x.detrend)
+x.detrend.scaled <- x.detrend / 20
 autoplot(x.detrend.scaled)+xlab("Time")+ylab("Value")
 acfx <- acf(x.detrend.scaled)
 acfx <- ts(acfx$acf)
@@ -40,16 +42,14 @@ theme(axis.title=element_text(size=18))+
 
 N.arma = 15
 aic.ar = rep(NA, N.arma)
-for (n in 5:N.arma) {
+for (n in 1:N.arma) {
   fit <- arima(x.detrend.scaled, order = c(n,0,0), include.mean=FALSE)
   aic.ar[n] <- fit$aic
 }
-plot(5:N.arma, aic.ar[5:N.arma], type="l")
+plot(1:N.arma, aic.ar[1:N.arma], type="l", ylab="Aikake Information Criterion", xlab="order of AR")
+plot(5:N.arma, aic.ar(5:N.arma), type="l", ylab="Aikake Information Criterion", xlab="order of AR")
 
-arima(x.detrend.scaled, order = c(8,0,0), include.mean=FALSE)
-arima(x.detrend.scaled, order = c(8,0,1), include.mean=FALSE)
-
-K.arma = 30
+K.arma = 5
 aic.ma = rep(NA, K.arma)
 for (k in 1:K.arma) {
   fit <- arima(x.detrend.scaled, order = c(0,0,k), include.mean=FALSE)
@@ -57,26 +57,16 @@ for (k in 1:K.arma) {
 }
 plot(1:K.arma, aic.ma, type="l")
 
-aic(arima(x.detrend.scaled, order = c(0,0,1), include.mean=FALSE)) # 6474.29
-arima(x.detrend.scaled, order = c(0,0,2), include.mean=FALSE) # 5892.73
-arima(x.detrend.scaled, order = c(0,0,3), include.mean=FALSE) # 5833.16
-arima(x.detrend.scaled, order = c(0,0,4), include.mean=FALSE) # 5857.36
-arima(x.detrend.scaled, order = c(0,0,5), include.mean=FALSE) # 5704.51
-arima(x.detrend.scaled, order = c(0,0,6), include.mean=FALSE) # 5460.69
-arima(x.detrend.scaled, order = c(0,0,7), include.mean=FALSE) # 5454.32
-arima(x.detrend.scaled, order = c(0,0,8), include.mean=FALSE) # 5386.22
-arima(x.detrend.scaled, order = c(0,0,9), include.mean=FALSE) # 5369.43
-arima(x.detrend.scaled, order = c(0,0,10), include.mean=FALSE) # 5297.06
-arima(x.detrend.scaled, order = c(0,0,11), include.mean=FALSE) # 5277.56
-arima(x.detrend.scaled, order = c(0,0,12), include.mean=FALSE) # 5233.7
-arima(x.detrend.scaled, order = c(0,0,13), include.mean=FALSE) # 5235.59
-arima(x.detrend.scaled, order = c(0,0,14), include.mean=FALSE) # 5213.55
-arima(x.detrend.scaled, order = c(0,0,15), include.mean=FALSE) # 5190.74
-arima(x.detrend.scaled, order = c(0,0,16), include.mean=FALSE) # 5172.24
+arima(x.detrend.scaled, order = c(6,0,0), fixed=c(NA,0,NA,NA,NA,NA), include.mean=FALSE)
 
-
-arima(x.detrend.scaled, order = c(1,0,0), include.mean=FALSE)
-arima(x.detrend.scaled, order = c(2,0,0), include.mean=FALSE)
-arima(x.detrend.scaled, order = c(3,0,0), include.mean=FALSE)
-arima(x.detrend.scaled, order = c(1,0,1), include.mean=FALSE)
+max.order <- 5
+1:1
+for (n in 0:max.order) {
+  for (k in 0:max(0, max.order-n)) {
+    c(n,0,k)
+    print(n)
+    print(k)
+    print(arima(x.detrend.scaled, order = c(n,0,k), include.mean = FALSE)$aic)
+  }
+}
 
